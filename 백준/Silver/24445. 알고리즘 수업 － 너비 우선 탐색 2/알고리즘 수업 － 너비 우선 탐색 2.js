7c1c8eb794b4f5aaadf853d8059c90b374cd6441
Kimyebin00 <70912819+Kimyebin00@users.noreply.github.com>
2024-09-28 22:32:29 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 1144 ms, Memory: 90072 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.js
@@ -0,0 +1,43 @@
+function solution() {
+  const fs = require('fs');
+  const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+  const [n, m, r] = input[0].split(" ").map(Number);
+
+  const graph = Array.from({length: n+1}, () => []);
+
+  for (let i = 1; i <= m; i++) {
+    const [a, b] = input[i].split(" ").map(Number);
+
+    graph[a].push(b);
+    graph[b].push(a);
+  }
+  
+  graph.forEach(nodes => nodes.sort((a,b) => b-a));
+
+  const answer = Array(n+1).fill(0);
+  const visited = Array(n+1).fill(false);
+
+  const nodes = [r];
+  visited[r] = true;
+  answer[r] = 1;
+
+  let count = 2;
+  while (nodes.length !== 0) {
+    let currentNode = nodes.shift();
+
+    for (const nextNode of graph[currentNode]) {
+      if (!visited[nextNode]) {
+        visited[nextNode] = true;
+        answer[nextNode] = count++;
+        nodes.push(nextNode);
+      }
+    }
+  }
+
+  answer.shift();
+
+  console.log(answer.join('\n'));
+}
+
+solution();
