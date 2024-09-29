88ff3a24f0b1008123b9bbe8305f85c5770bfdff
Kimyebin00 <70912819+Kimyebin00@users.noreply.github.com>
2024-09-29 22:07:28 +0900

    [Silver V] Title: 타일 장식물, Time: 96 ms, Memory: 9344 KB, Score: 74 point -BaekjoonHub

---
+++ 백준/Silver/13301. 타일 장식물/타일 장식물.js
@@ -0,0 +1,19 @@
+function solution() {
+  const fs = require('fs');
+  const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+  
+  const n = +input[0];
+  
+  const dp = Array.from({length: n}, () => []);
+
+  dp[0] = [1,1];
+
+  for (let i = 1; i < n; i++) {
+    if (i % 2) dp[i] = [dp[i-1][0] + dp[i-1][1], dp[i-1][1]];
+    else dp[i] = [dp[i-1][0], dp[i-1][0] + dp[i-1][1]]
+  }
+
+  console.log((dp[n-1][0] + dp[n-1][1]) * 2);
+}
+
+solution();
